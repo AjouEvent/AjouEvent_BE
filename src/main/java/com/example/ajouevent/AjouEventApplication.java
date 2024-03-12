74f9d40f29dc0f56dac3cc27e2066e2f8293a62b
Simzard10 <jysim0326@ajou.ac.kr>
2024-03-13 02:12:50 +0900

    main 함수

---
+++ src/main/java/com/example/ajouevent/AjouEventApplication.java
@@ -0,0 +1,18 @@
+package com.example.ajouevent;
+
+import org.apache.catalina.connector.Connector;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class AjouEventApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AjouEventApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ajouevent/AjouEventApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ajouevent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AjouEventApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
