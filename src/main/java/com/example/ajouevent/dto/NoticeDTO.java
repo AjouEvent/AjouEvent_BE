b867612412a603612ba4c23deadc3254ada07321
Simzard10 <jysim0326@ajou.ac.kr>
2024-04-16 23:00:48 +0900

    add : Webhook에서 크롤링 서버에게 요청받는 NoticeDTO 추가

---
+++ src/main/java/com/example/ajouevent/dto/NoticeDTO.java
@@ -0,0 +1,38 @@
+package com.example.ajouevent.dto;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import com.example.ajouevent.domain.Type;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class NoticeDTO {
+
+	@NotNull(message = "제목은 Null 일 수 없습니다!")
+	private String title;
+
+	private String content;
+
+	private String category;
+
+	private String department;
+
+	private String englishTopic;
+
+	private String koreanTopic;
+
+	private String url;
+
+	private List<String> images;
+
+	private LocalDateTime date;
+
+
+}
