a090875b05772fc4ef83db4a0da8f924bf3821c6
Simzard10 <jysim0326@ajou.ac.kr>
2024-04-18 17:22:16 +0900

    refactor : TopicService 의존성으로 수정

--- src/main/java/com/example/ajouevent/controller/TopicController.java
+++ src/main/java/com/example/ajouevent/controller/TopicController.java
@@ -1,7 +1,6 @@
 package com.example.ajouevent.controller;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,14 +8,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.example.ajouevent.domain.Token;
-import com.example.ajouevent.domain.Topic;
-import com.example.ajouevent.dto.ResponseDTO;
+import com.example.ajouevent.dto.ResponseDto;
 import com.example.ajouevent.dto.TopicRequest;
-import com.example.ajouevent.repository.TopicRepository;
 import com.example.ajouevent.service.FCMService;
 import com.example.ajouevent.service.TopicService;
 
@@ -31,25 +26,21 @@ public class TopicController {
 	private final TopicService topicService;
 
 	@PostMapping("/subscribe")
-	public ResponseEntity<ResponseDTO> subscribeToTopic(@RequestBody TopicRequest topicRequest) {
-		String topicName = topicRequest.getTopic();
-		List<Token> tokens = topicRequest.getTokens();
-
-		// topicService.subscribeToTopic(topicRequest);
-		fcmService.subscribeToTopic(topicName, tokens);
-		return ResponseEntity.ok().body(ResponseDTO.builder()
+	public ResponseEntity<ResponseDto> subscribeToTopic(@RequestBody TopicRequest topicRequest) {
+		topicService.subscribeToTopic(topicRequest);
+		return ResponseEntity.ok().body(ResponseDto.builder()
 			.successStatus(HttpStatus.OK)
-			.successContent(topicName +" 토픽을 구독합니다.")
+			.successContent(topicRequest.getTopic() +" 토픽을 구독합니다.")
 			.build()
 		);
 	}
 
 	@PostMapping("/unsubscribe")
-	public ResponseEntity<ResponseDTO> unsubscribeFromTopic(@RequestBody TopicRequest topicRequest) {
+	public ResponseEntity<ResponseDto> unsubscribeFromTopic(@RequestBody TopicRequest topicRequest) {
 		String topic = topicRequest.getTopic();
 		List<Token> tokens = topicRequest.getTokens();
 		fcmService.unsubscribeFromTopic(topic, tokens);
-		return ResponseEntity.ok().body(ResponseDTO.builder()
+		return ResponseEntity.ok().body(ResponseDto.builder()
 			.successStatus(HttpStatus.OK)
 			.successContent(topic +" 토픽을 구독 취소합니다.")
 			.build()
