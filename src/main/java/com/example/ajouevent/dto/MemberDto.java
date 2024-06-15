9dc5d17a4ce52cf01275fc7e4ac30f2ecb6b5bc7
Simzard10 <jysim0326@ajou.ac.kr>
2024-06-15 16:58:52 +0900

    refactor : Dto에 @NoArgsConstructor            @AllArgsConstructor            @Builder 추가

--- src/main/java/com/example/ajouevent/dto/MemberDto.java
+++ src/main/java/com/example/ajouevent/dto/MemberDto.java
@@ -3,12 +3,16 @@ package com.example.ajouevent.dto;
 import com.example.ajouevent.domain.Role;
 import lombok.*;
 
+@Getter
+@Setter
 public class MemberDto {
 
 	@Data
 	@Builder
 	@AllArgsConstructor
-	@NoArgsConstructor(access = AccessLevel.PROTECTED)
+	@NoArgsConstructor
+	@Getter
+	@Setter
 	public static class LoginRequest {
 
 		private String email;
@@ -18,21 +22,14 @@ public class MemberDto {
 	}
 
 	@Getter
-	@NoArgsConstructor(access = AccessLevel.PROTECTED)
+	@NoArgsConstructor
+	@AllArgsConstructor
 	@Builder
 	public static class MemberInfoDto {
 		private Long memberId;
 		private String email;
 		private String password;
 		private Role role;
-
-		@Builder
-		public MemberInfoDto(Long memberId, String email, String password, Role role) {
-			this.memberId = memberId;
-			this.email = email;
-			this.password = password;
-			this.role = role;
-		}
 
 	}
 }
--- src/main/java/com/example/ajouevent/dto/NoticeDto.java
+++ src/main/java/com/example/ajouevent/dto/NoticeDto.java
@@ -6,12 +6,16 @@ import java.util.List;
 import com.example.ajouevent.domain.ClubEventImage;
 
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class NoticeDto {
 
