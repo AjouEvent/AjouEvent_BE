a63f184f0e9b0f11641aab50c06e1aa74d51d99e
심재엽 <jysim0326@ajou.ac.kr>
2024-07-17 20:40:45 +0900

    refactor : 인기글 조회 API Redis 캐싱, 이번주에 올라온 게시글에 대한 인기글로 리팩토링

--- src/main/java/com/example/ajouevent/repository/EventRepository.java
+++ src/main/java/com/example/ajouevent/repository/EventRepository.java
@@ -1,10 +1,12 @@
 package com.example.ajouevent.repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
